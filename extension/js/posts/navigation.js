2173deb7009ee03cdb802b6dc4d0cf20f175ce4d
Stefan Kendall <stefankendall@gmail.com>
2013-01-13 16:26:32 -0500

    Added submit automation.

--- extension/js/posts/navigation.js
+++ extension/js/posts/navigation.js
@@ -1,29 +1,36 @@
 (function () {
-    var mainPageRedirection = function(){
+    var mainPageRedirection = function () {
         if (location.href.slice(-4) === 'org/') {
             location.href = "bka";
         }
     };
 
-    var postInCategory = function(){
+    var postInCategory = function () {
         if (location.href.slice(-4) === 'bka/') {
             location.href = $('a:contains("post")').first().attr('href');
         }
     };
 
-    var chooseCategory = function(){
+    var chooseCategory = function () {
         if ($('b').text().indexOf("Please choose a category") !== -1) {
             $('label:contains("books & magazines - by owner")').find('input').attr('checked', true);
             $('button:contains("Continue")').click();
         }
     };
 
-    var doneWithImages = function(){
+    var doneWithImages = function () {
         $('button:contains("Done with Images")').click();
+    };
+
+    var submit = function () {
+        if ($('div').text().indexOf('confirm your ad') !== -1) {
+            $('button:contains("Continue")').click();
+        }
     };
 
     mainPageRedirection();
     postInCategory();
     chooseCategory();
     doneWithImages();
+    submit();
 })();
