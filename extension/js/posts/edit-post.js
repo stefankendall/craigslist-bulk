897f91d6f1c86f800336a03850c9d4f478397571
Stefan Kendall <stefankendall@gmail.com>
2013-01-14 20:19:23 -0500

    Fixing apostrophe

--- extension/js/posts/edit-post.js
+++ extension/js/posts/edit-post.js
@@ -4,7 +4,7 @@
         price:"$9.99",
         description:("Using nothing but craigslist, I made $1000 in 1 month. NO special skills or knowledge are required.\n\n" +
         "I've created a book that walks through every step of the process and every pitfall so you can get up and running as quickly as possible.\n\n" +
-        "I've already made $500 this week making trades, and you can too. If you want to make some serious money, contact me and I’ll get you the book.")
+        "I've already made $500 this week making trades, and you can too. If you want to make some serious money, contact me and I'll get you the book.")
     };
 
     var findInputFor = function (text) {
